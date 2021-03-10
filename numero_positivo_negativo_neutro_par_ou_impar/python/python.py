5594bfab2ef21f12f828c3c332f7a20b2f22a044
Lucca <71888383+Luccazx12@users.noreply.github.com>
2021-03-09 23:28:11 -0300

    Add files via upload

---
+++ numero_positivo_negativo_neutro_par_ou_impar/python/python.py
@@ -0,0 +1,18 @@
+#Entrada
+n = int(input('Digite um valor: '))
+
+#Processamento e Saídas
+if n > 0:
+    print('Seu número é positivo.')
+
+elif n < 0:
+    print('Seu número é negativo.')
+
+else:
+    print('Seu número é neutro.')
+
+if n%2 ==0:
+    print('Seu número é par')
+
+else:
+    print('Seu número é impar')
